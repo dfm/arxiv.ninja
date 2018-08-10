f6ed710d2399945246de9f20902b355ee9ae7e74
Dan F-M <danfm@nyu.edu>
2018-08-10 13:31:02 +0200

    caching

--- script.py
+++ script.py
@@ -4,32 +4,51 @@
 from __future__ import division, print_function
 
 import os
+import sys
 import random
 import requests
 from copy import deepcopy
 from lxml import html, etree
 
-path = "list/astro-ph/new"
+if len(sys.argv) >= 2:
+    paths = sys.argv[1:]
+else:
+    with open("paths.txt", "r") as f:
+        paths = f.read().splitlines()
 
-content = requests.get("https://arxiv.org/{0}".format(path)).text
-content = content.replace("href=\"/", "href=\"http://arxiv.org/")
-tree = html.fromstring(content.encode("utf-8"))
+cache_path = "cache"
+for path in paths:
+    # Attempt to load from cache
+    fn = cache_path + path
+    os.makedirs(fn, exist_ok=True)
+    fn = os.path.join(fn, "index.html")
 
-for block in tree.xpath('//div[@id="dlpage"]/dl'):
-    entries = []
-    for inner in block:
-        if inner.tag == "dt":
-            entries.append([deepcopy(inner), None])
-        else:
-            entries[-1][1] = deepcopy(inner)
-        inner.drop_tree()
+    if not os.path.exists(fn):
+        r = requests.get("https://arxiv.org{0}?show=500".format(path))
+        with open(fn, "wb") as f:
+            f.write(r.content)
 
-    random.shuffle(entries)
+    with open(fn, "rb") as f:
+        content = f.read().decode("utf-8")
 
-    for entry in entries:
-        block.append(entry[0])
-        block.append(entry[1])
+    content = content.replace("href=\"/", "href=\"http://arxiv.org/")
+    tree = html.fromstring(content.encode("utf-8"))
 
-path = os.path.join("output", path)
-os.makedirs(path, exist_ok=True)
-open(os.path.join(path, "index.html"), "wb").write(etree.tostring(tree))
+    for block in tree.xpath('//div[@id="dlpage"]/dl'):
+        entries = []
+        for inner in block:
+            if inner.tag == "dt":
+                entries.append([deepcopy(inner), None])
+            else:
+                entries[-1][1] = deepcopy(inner)
+            inner.drop_tree()
+
+        random.shuffle(entries)
+
+        for entry in entries:
+            block.append(entry[0])
+            block.append(entry[1])
+
+    path = os.path.join("output", path[1:])
+    os.makedirs(path, exist_ok=True)
+    open(os.path.join(path, "index.html"), "wb").write(etree.tostring(tree))
